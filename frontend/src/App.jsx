0eeee1d19f648da6525a8d4af76ccf43c57d47f1
sarankumar <sarankumar742001@gmail.com>
2025-10-25 07:25:52 +0530

    user gigs page are added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,28 +5,13 @@ import Login from "./pages/Auth/login";
 import Register from "./pages/Auth/Register";
 import Home from "./pages/Auth/Home";
 import ProtectedRoute from "./pages/Auth/protectedRoute";
+import AllGigs from "./pages/gigs/allGigs";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* ---------- Public Routes ---------- */}
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-        {/* ---------- Protected Routes ---------- */}
-        <Route
-          path="/home"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* ---------- Catch-all ---------- */}
-        <Route path="*" element={<h2>404 Page Not Found</h2>} />
+       <Route path="/allgigs" element={<AllGigs/>}/>
       </Routes>
     </Router>
   );
