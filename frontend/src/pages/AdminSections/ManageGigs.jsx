36ba8ec0cf0bb7e4bc5464df993154f267e1638f
sarankumar <sarankumar742001@gmail.com>
2025-10-31 12:12:20 +0530

    all codes are removed from the branch

--- frontend/src/pages/AdminSections/ManageGigs.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ManageGigs() {
-  return <div>ManageGigs</div>;
-}
-
-export default ManageGigs;
--- frontend/src/pages/AdminSections/Overview.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Overview = () => (
-  <div>
-    <h2>Overview</h2>
-    <p>This is the overview section for admins.</p>
-  </div>
-);
-
-export default Overview;
