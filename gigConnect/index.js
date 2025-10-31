2d731bc66861568ea769069840a4ec6f0a7b68fc
sarankumar <sarankumar742001@gmail.com>
2025-10-31 17:10:16 +0530

    all files deleted

--- gigConnect/index.js
+++ gigConnect/index.js
@@ -6,19 +6,3 @@ c= a+b
 console.log(c)
 d = a+b+c
 console.log(d)
-
-
-function countAlpha(str){
-    let freq = {}
-    for(let char of str.toLowerCase()){
-    
-    if(/[a-z]/.test(char)){
-        freq[char]=(freq[char] || 0)+1
-
-    }
-}
-    return freq
-    }
-let input='sarankumarroseqwtyredcsx'
-let output = countAlpha(input)
-console.log(output)
