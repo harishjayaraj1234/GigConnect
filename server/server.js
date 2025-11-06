78de80bc1b537f6ca3a784a190d6f0c21c245d12
Sarankumar Muniyappillai <sarankumar742001@gmail.com>
2025-11-06 11:40:12 +0530

    Update CORS origin to new Vercel URL

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ connectDB()
 const server = createServer(app); 
 const io = new Server(server, {
     cors: {
-        origin : "https://gigconnect-dev.onrender.com",
+        origin : "https://gig-connect-delta.vercel.app/",
         methods : ["GET", "POST"],
         credentials : true
     }
@@ -38,7 +38,7 @@ socketHandler(io);
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://gigconnect-dev.onrender.com',  
+  origin: "https://gig-connect-delta.vercel.app/",  
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true               
 }));
