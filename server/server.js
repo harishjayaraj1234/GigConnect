006ccc9cb3686c92d86adcf172d5f06e90a721cc
nitin2353 <154704828+nitin2353@users.noreply.github.com>
2025-11-06 20:29:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ socketHandler(io);
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",  
+  origin: "https://gigconnect-ten.vercel.app",  
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true               
 }));
