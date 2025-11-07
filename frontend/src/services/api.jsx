0491d2fff546893288583ea3aee9cd6cedc64b73
Sarankumar Muniyappillai <sarankumar742001@gmail.com>
2025-11-07 13:25:25 +0530

    Change API base URL to production server

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend base
+  baseURL: "https://gigconnect-server-ri5s.onrender.com/api", // backend base
   withCredentials: true, // optional (for cookies / auth)
 });
 
