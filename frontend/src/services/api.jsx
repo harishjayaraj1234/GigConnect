73311a9eea890f3f944c58c5ff51e23b07274e2a
Sarankumar Muniyappillai <sarankumar742001@gmail.com>
2025-11-07 14:43:36 +0530

    Update API base URL to use environment variable

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://gigconnect-server-ri5s.onrender.com/api", // backend base
+  baseURL: import.meta.env.VITE_API_URL/api, // backend base
   withCredentials: true, // optional (for cookies / auth)
 });
 
