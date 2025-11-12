8515a51aaf5af6816c4b8eb51b47c39e8fe17949
Sarankumar Muniyappillai <sarankumar742001@gmail.com>
2025-11-12 06:54:09 +0530

    Update CORS origin to localhost for development

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ connectDB()
 const server = createServer(app); 
 const io = new Server(server, {
     cors: {
-        origin : process.env.CLIENT_URL,
+        origin : ["http://localhost:5173"],
         methods : ["GET", "POST"],
         credentials : true
     }
@@ -39,7 +39,7 @@ socketHandler(io);
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL,  
+  origin: "http://localhost:5173",  
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true               
 }));
